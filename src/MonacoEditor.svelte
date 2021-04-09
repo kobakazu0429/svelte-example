41ad7de76d0ae0bb0bbfecb6a64cd548bfb719f6
kobakakzu0429 <kobakazu10@gmail.com>
2021-04-09 22:57:23 +0900

    update

--- src/MonacoEditor.svelte
+++ src/MonacoEditor.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
   import loader from "@monaco-editor/loader";
+  import { fib } from "./hoge";
 
+  loader.config({ "vs/nls": { availableLanguages: { "*": "ja" } } });
   loader.init().then((monaco) => {
-    monaco.editor.create(document.querySelector("#app"), {
-      value: "// some comment",
+    const newEditor = monaco.editor.create(document.querySelector("#app")!, {
+      value: [...Array(10).keys()].map((i) => fib(i)).join(", "),
       language: "c",
       theme: "vs-dark",
       scrollbar: {
@@ -11,12 +13,29 @@
       },
       fontSize: 16,
       wordWrap: "on",
-      wordWrapMinified: true,
       minimap: {
         enabled: false,
       },
       lineNumbers: "on",
     });
+    newEditor.updateOptions({ tabSize: 2 });
+    newEditor.onDidChangeModelContent((_event: any) => {
+      const value = newEditor.getValue();
+      // props.onChangeValue(value);
+    });
+    console.log(monaco.languages.getLanguages());
+    // @ts-ignore
+    console.log(newEditor._themeService._knownThemes);
+    // monaco.editor.defineTheme("One Dark Pro")
+    // const rect = editorRef.current.getBoundingClientRect();
+    // newEditor.layout({ width: rect.width, height: rect.height });
+    // props.focusWithDidMount && newEditor.focus();
+
+    // return () => {
+    //   // const p = newEditor.getPosition();
+    //   // const offset = newEditor.getOffsetForColumn(p.lineNumber, p.column);
+    //   newEditor.dispose();
+    // };
   });
 </script>
 
