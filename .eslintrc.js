d5b310e0e5c52c93d163a63b99545ba6812aec15
kobakakzu0429 <kobakazu10@gmail.com>
2021-04-10 10:39:54 +0900

    use @kobakazu0429/eslint-config-typescript-svelte

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,40 +1,7 @@
 module.exports = {
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
-  ],
-  parser: "@typescript-eslint/parser",
+  extends: "@kobakazu0429/eslint-config-typescript-svelte",
   parserOptions: {
-    ecmaVersion: 2019,
-    sourceType: "module",
+    project: './tsconfig.json',
     tsconfigRootDir: __dirname,
-    project: ["./tsconfig.json"],
-  },
-  env: {
-    es6: true,
-    node: true,
-    browser: true,
-  },
-  settings: {
-    "svelte3/typescript": require("typescript"),
-  },
-  plugins: ["svelte3", "@typescript-eslint"],
-  overrides: [
-    {
-      files: ["**/*.svelte"],
-      processor: "svelte3/svelte3",
-    },
-  ],
-  rules: {
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/ban-types": "off",
-    "@typescript-eslint/ban-ts-comment": "off",
-    "@typescript-eslint/no-non-null-assertion": "off",
-    // for svelte
-    "@typescript-eslint/consistent-type-imports": [
-      "error",
-      { prefer: "type-imports" },
-    ],
   },
 };
